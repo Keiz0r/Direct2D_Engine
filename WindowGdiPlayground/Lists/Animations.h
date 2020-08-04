b196a217b52480557487617e03bfafdb8ac40604
Keizi <keizisc2@gmail.com>
2020-08-04 18:21:54 +0300

    p

--- WindowGdiPlayground/Lists/Animations.h
+++ WindowGdiPlayground/Lists/Animations.h
@@ -1,6 +1,6 @@
 #pragma once
 //												BatchStartx		BatchStarty		Width		Height		Stride	  AnimationRows	   FramesInARow		FrameTime
-#define SONIC_ANIMATION_IDLE						 0.0f,			0.0f,		45.0f,		 46.0f, 	  0.0f,		   		 1,				16u,		  40u
+#define SONIC_ANIMATION_IDLE						 393.0f,	      0.0f,		65.0f,		 60.0f, 	  0.0f,		   		 1,				 1u,		  40u
 #define SONIC_ANIMATION_RUN_N						 0.0f,			951.0f,		65.0f,		 59.0f, 	  0.0f,				 2,		   	     6u,		  10u
 #define SONIC_ANIMATION_RUN_SW_NE					 0.0f,			471.0f,		65.0f,		 58.0f, 	  0.0f,				 2,		   	     6u,		  10u				
 #define SONIC_ANIMATION_RUN_E						 0.0f,			471.0f,		65.0f,		 58.0f, 	  0.0f,				 2,		   	     6u,		  10u
